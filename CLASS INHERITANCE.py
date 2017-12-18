2f99fc444b265511a42e0d3431dc1741672405a5
harsh chaurasiya <harshchaurasiya6768@gmail.com>
2017-12-18 15:06:35 +0530

    Update CLASS INHERITANCE.py

--- CLASS INHERITANCE.py
+++ CLASS INHERITANCE.py
@@ -1,21 +1,21 @@
-class vehicle():
-    def __init__(self,name,color):
-        self.__name=name        # private member
-        self.__color=color
-    def getcolor(self):
-        return self.__color
-    def setcolor(self,color):
-        self.__color=color
-    def getname(self):
-        return self.__name
+class vehicle():  # parent class
+    def __init__(self):
+        self.__name = input("enter the name of the car:")  # private member
+        self.__color = input("enter the color of the car:")
 
-class Car(vehicle):      #child class
-    def __init(self,name,color,model):
-        super(Car,self).__init__(name,color)                # call the parent
-        self.__model=model
-    def getDedcription(self):
-        return self.getname()+self.__model+"in "+self.getcolor()+"color"
 
-c=Car("Ford Mustang","REd","GT350")
-print(c.getDescription())
-print(c.getname())
+class Car(vehicle):         # child class
+    def __init__(self):
+        super(Car, self).__init__()                        # call the parent
+        self.__model = input("enter the model of the car:")
+
+    def getDescription(self):
+        print("Description of the Car")
+        return self._vehicle__name + " " + self.__model + " in " + self._vehicle__color + " color!"
+
+def main():
+    c = Car()
+    print(c.getDescription())
+
+if __name__ == "__main__":
+    main()
